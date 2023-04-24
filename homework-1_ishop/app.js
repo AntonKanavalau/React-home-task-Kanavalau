e96760353efb38b351ee00723db55f6077fc6233
Anton Kanavalau <old.projecta@gmail.com>
2023-04-24 10:23:38 +0300

    add table header and style

--- homework-1_ishop/app.js
+++ homework-1_ishop/app.js
@@ -12,25 +12,31 @@ var myComponent = React.createClass({
         count: React.PropTypes.number.isRequired,
         code: React.PropTypes.number.isRequired,
       })
-    )
+    ),
+    tableHeader: React.PropTypes.array.isRequired,
   },
 
   render: function () {
 
 
-
-    var products = this.props.productList.map( char =>
-    React.DOM.tr({key:char.code, className: 'Row'},
-      React.DOM.td({className: 'Title'}, char.title),
-      React.DOM.td({className: 'Price'}, char.price + 'р.'),
-      React.DOM.td({className: 'IMG'}, React.DOM.img({src: char.URL, alt: 'Product image'})),
-      React.DOM.td({className: 'Count'}, char.count + 'шт.'),
+    var products = this.props.productList.map(char =>
+      React.DOM.tr({key: char.code, className: 'Row'},
+        React.DOM.td({className: 'Title'}, char.title),
+        React.DOM.td({className: 'Price'}, char.price + 'р.'),
+        React.DOM.td({className: 'IMG'}, React.DOM.img({src: char.URL, alt: 'Product image'})),
+        React.DOM.td({className: 'Count'}, char.count + 'шт.'),
       )
     );
 
-    return React.DOM.div( null,
+    var tableHeader = this.props.tableHeader.map(char =>
+        React.DOM.th({className: 'headTitle'}, char),
+    );
+
+    return React.DOM.div(null,
       React.DOM.h1({className: 'headerTitle'}, this.props.headerTitle),
-      React.DOM.table({className: 'myComponentBlock'}, React.DOM.tbody(null, products))
+      React.DOM.table({className: 'myComponentBlock'},
+        React.DOM.thead(null, React.DOM.tr({className: 'header'}, tableHeader)),
+        React.DOM.tbody({className: 'tableBody'}, products))
     );
   }
 
