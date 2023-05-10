9d0d9fe07746c98573c956e9f9ab9a3246403519
Anton Kanavalau <old.projecta@gmail.com>
2023-05-10 19:26:01 +0300

    work with npm

---
+++ homework-3_ishop3/app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/*все для реакта*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*закидываем стили*/
+import './index.css';
+
+/*родительский компонент*/
+import shopComponent from './components/shop';
+
+/*заголовок на странице*/
+let shopTitle = "Магазин техники";
+
+/*массив товаров и заголовков таблицы*/
+import productsArray from './productsArray.json';
+import tableHeader from './tableHeader.json';
+
+/*рендерим*/
+ReactDOM.render(
+  React.createElement(shopComponent, {tableHeader: tableHeader, headerTitle: shopTitle, productList:
+    productsArray}),
+  document.getElementById('container')
+);
