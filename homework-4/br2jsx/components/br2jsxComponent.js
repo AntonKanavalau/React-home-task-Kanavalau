9f23366412a226086368cf799f2dbff8ed2100e6
Anton Kanavalau <old.projecta@gmail.com>
2023-06-01 11:28:22 +0300

    updare br2jsxComponent

--- homework-4/br2jsx/components/br2jsxComponent.js
+++ homework-4/br2jsx/components/br2jsxComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const Br2jsx = ({text}) => {
+  let arr = text.split(/<br ?\/?>/)
+                .flatMap((line, i) => [line, <br key={`line-${i}`} />]);
   return <div key={1}
-              style={{width: "200px",textAlign: "center",color: "white", backgroundColor: "green", padding: "10px"}}>{text}</div>
+              style={{width: "200px",textAlign: "center",color: "white", backgroundColor: "green", padding: "10px"}}>{arr}</div>
 }
 
 export default Br2jsx;
